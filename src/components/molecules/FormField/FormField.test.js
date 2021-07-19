2a730e3177b5a01abd322ab0acfba5c69bbaa3be
Grzegorz <grzegorztarkowski8@wp.pl>
2021-07-19 22:52:42 +0200

    After Episode 9

---
+++ src/components/molecules/FormField/FormField.test.js
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+import React, { useState } from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import FormField from './FormField';
+import { renderWithProviders } from 'helpers/renderWithThemeProvider';
+
+describe('Form Field', () => {
+  it('Renders the component', () => {
+    renderWithProviders(<FormField label="name" name="name" id="name" />);
+  });
+});
